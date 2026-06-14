8f8334ace417de67d5b51c80a7230b279b67bb08
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-14 08:43:25 +0530

    added the authroutes page and its code and also designed the server.js page accordingly

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { connection } from "./config/dbconfig.js"
+import cookieParser from "cookieparser"
+import authRoutes from "./routes/authRoutes.js"
+
+dotenv.config()
+
 const app =express()
-let port = 5000
+
+const port = process.env.port || 5000
 
 app.use(express.json())
+app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    res.send("This is landing page")
-})
+app.use("/api/auth",authRoutes)
 app.listen(port,(req,res)=>{
     console.log(`app is listening on port ${port}`)
 })
